0338d7bbd8b8388b4cf4d6a25e05251d11a6de38
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-23 18:38:01 +0900

    save

--- source/_layouts/blog.blade.php
+++ source/_layouts/blog.blade.php
@@ -11,6 +11,24 @@
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}"> --}}
         {{-- <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script> --}}
 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css">
+
+        <style>
+            .markdown-body {
+                box-sizing: border-box;
+                min-width: 200px;
+                max-width: 980px;
+                margin: 0 auto;
+                padding: 45px;
+            }
+
+            @media (max-width: 767px) {
+                .markdown-body {
+                    padding: 15px;
+                }
+            }
+        </style>
+
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://unpkg.com/alpinejs" defer></script>
     </head>
@@ -19,7 +37,15 @@
 
         @include('_partials.nav')
 
-        @yield('body')
+        <div class="markdown-body">
+
+            <h2 class="mb-2">{{ $page->title }}</h2>
+            <h4 class="mb-2">Author : {{ $page->author }}</h4>
+
+            @yield('body')
+
+        </div>
+
     </body>
 
 </html>
